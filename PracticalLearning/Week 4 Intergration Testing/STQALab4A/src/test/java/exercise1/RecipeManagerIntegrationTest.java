a603d87007888668c89d2fe4a86e23743f3a4502
j34-chapman <jakechapman710@gmail.com>
2023-11-17 15:30:58 +0000

    work

--- PracticalLearning/Week 4 Intergration Testing/STQALab4A/src/test/java/exercise1/RecipeManagerIntegrationTest.java
+++ PracticalLearning/Week 4 Intergration Testing/STQALab4A/src/test/java/exercise1/RecipeManagerIntegrationTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RecipeManagerIntegrationTest {
 
@@ -42,12 +41,44 @@ class RecipeManagerIntegrationTest {
     @DisplayName("Test retrieval of non-existent recipe")
     void testRetrievalOfNonExistentRecipe_ShouldReturnNull() {
         // todo: finish implementation
+
+        //act
+        Recipe chicken = recipeManager.getRecipe("Chicken");
+
+        //assert
+        assertNull(chicken, "The retrieved recipe doesnt exist");
+
     }
 
     // Exercise 1.3 - Test that duplicate recipe names retrieve the first occurrence
+    /* Is it better not sometimes to fail test for example , in this the test passes as it should do be if we
+    are to fail the test on purpouse : eg. we change the assertEquals to ingredients to hot sauce , is this not a
+    better demonstration that the intergation is working as it should be because surely we can create a test that passes
+    but doesnt actually act the way it shoould do , however also vice versa how do we know if a tests fail becuase of
+    the test code we written is syntaxly/not written correctly or the way the they have written there code doesnt act they
+    way it should !
+     */
+
     @Test
     @DisplayName("Adding recipes with duplicate names should retrieve the first added recipe on search")
     void testRecipesWithDuplicateNamesShouldRetrieveFirstAdded() {
         // todo: finish implementation
+
+        // arrange
+        Recipe recipe1 = new Recipe("Fried Chicken", "flour, milk, eggs", "Mix ingredients and cook.");
+        // arrange
+        Recipe recipe2 = new Recipe("Fried Chicken", "Hot Sauce, milk, eggs", "Mix ingredients and cook.");
+
+        //Act
+        recipeManager.addRecipe(recipe1);
+        recipeManager.addRecipe(recipe2);
+        Recipe retrievedRecipe = recipeManager.getRecipe("Fried Chicken");
+
+        //assert
+        assertNotNull(retrievedRecipe, "The retrieved recipe should not be null");
+        assertEquals("Fried Chicken", retrievedRecipe.getName(), "The retrieved recipe should have the correct name");
+        assertEquals("flour, milk, eggs", retrievedRecipe.getIngredients(), "The retrieved recipe should have the correct ingredients");
+        assertEquals("Mix ingredients and cook.", retrievedRecipe.getInstructions(), "The retrieved recipe should have the correct instructions");
+
     }
 }
